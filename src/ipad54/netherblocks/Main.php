ab59919b094592536d3aaefe3f660b6de28cb536
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-03-19 13:45:19 +1000

    Update Main.php

--- src/ipad54/netherblocks/Main.php
+++ src/ipad54/netherblocks/Main.php
@@ -87,7 +87,7 @@ use pocketmine\scheduler\AsyncTask;
 use pocketmine\Server;
 use pocketmine\utils\Config;
 use ReflectionMethod;
-use const pocketmine\src\data\Bedrock;
+use const pocketmine\DATA\BEDROCK;
 
 class Main extends PluginBase
 {
@@ -137,7 +137,7 @@ class Main extends PluginBase
 		$method = new ReflectionMethod(RuntimeBlockMapping::class, "registerMapping");
 		$method->setAccessible(true);
 
-		$blockIdMap = json_decode(file_get_contents(SRC\DATA\BEDROCK . 'block_legacy_id_map.json'), true);
+		$blockIdMap = json_decode(file_get_contents(DATA\BEDROCK . 'block_legacy_id_map.json'), true);
 		$metaMap = [];
 
 		foreach ($instance->getBedrockKnownStates() as $runtimeId => $nbt) {
