5686fd2637800d40236909700b952bb99f1eb9c4
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-03-19 11:40:49 +1000

    Update Main.php

--- src/ipad54/netherblocks/Main.php
+++ src/ipad54/netherblocks/Main.php
@@ -137,7 +137,7 @@ class Main extends PluginBase
 		$method = new ReflectionMethod(RuntimeBlockMapping::class, "registerMapping");
 		$method->setAccessible(true);
 
-		$blockIdMap = json_decode(file_get_contents(BEDROCK_DATA_PATH . 'block_legacy_id_map.json'), true);
+		$blockIdMap = json_decode(file_get_contents(BEDROCK . 'block_legacy_id_map.json'), true);
 		$metaMap = [];
 
 		foreach ($instance->getBedrockKnownStates() as $runtimeId => $nbt) {
