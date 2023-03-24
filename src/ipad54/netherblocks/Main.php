514f4cc03513fc831b1f2ab3779bb906fe0424bb
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-03-24 18:47:50 +1000

    Update Main.php

--- src/ipad54/netherblocks/Main.php
+++ src/ipad54/netherblocks/Main.php
@@ -3,12 +3,12 @@
 namespace ipad54\netherblocks;
 
 use ipad54\netherblocks\blocks\Basalt;
-use ipad54\netherblocks\blocks\Deepslate;
 use ipad54\netherblocks\blocks\Blackstone;
 use ipad54\netherblocks\blocks\Campfire;
 use ipad54\netherblocks\blocks\Chain;
 use ipad54\netherblocks\blocks\ChiseledPolishedBlackstone;
 use ipad54\netherblocks\blocks\CryingObsidian;
+use ipad54\netherblocks\blocks\Deepslate;
 use ipad54\netherblocks\blocks\FloorSign;
 use ipad54\netherblocks\blocks\Fungus;
 use ipad54\netherblocks\blocks\GildedBlackstone;
@@ -173,9 +173,9 @@ class Main extends PluginBase
 		if ($cfg->isEnabledDebris()) {
 			$this->registerBlock(new Opaque(new BID(CustomIds::ANCIENT_DEBRIS_BLOCK, 0, CustomIds::ANCIENT_DEBRIS_ITEM), "Ancient Debris", new BlockBreakInfo(30, BlockToolType::PICKAXE, ToolTier::DIAMOND()->getHarvestLevel(), 6000)));
 		}
-		// if ($cfg->isEnabledDeepslate()) {
-		// 	$this->registerBlock(new Opaque(new BID(CustomIds::DEEPSLATE_BLOCK, 0, CustomIds::DEEPSLATE_ITEM), "Deepslate", new BlockBreakInfo(30, BlockToolType::PICKAXE, ToolTier::DIAMOND()->getHarvestLevel(), 4.2)));
-		 // }
+	 if ($cfg->isEnabledDeepslate()) {
+		 	$this->registerBlock(new Opaque(new BID(CustomIds::DEEPSLATE_BLOCK, 0, CustomIds::DEEPSLATE_ITEM), "Deepslate", new BlockBreakInfo(1.5, BlockToolType::PICKAXE, ToolTier::DIAMOND()->getHarvestLevel(), 4.2)));
+		  }
 		if ($cfg->isEnabledBasalt()) {
 			$this->registerBlock(new Basalt(new BID(CustomIds::BASALT_BLOCK, 0, CustomIds::BASALT_ITEM), "Basalt", new BlockBreakInfo(1.25, BlockToolType::PICKAXE, ToolTier::WOOD()->getHarvestLevel(), 4.2)));
 			$this->registerBlock(new PolishedBasalt(new BID(CustomIds::POLISHED_BASALT_BLOCK, 0, CustomIds::POLISHED_BASALT_ITEM), "Polished Basalt", new BlockBreakInfo(1.25, BlockToolType::PICKAXE, ToolTier::WOOD()->getHarvestLevel(), 4.2)));
