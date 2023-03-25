fe53825d358e5e98f15aef214440c3d00787a3bb
jasonwynn10 <jasonwynn10@me.com>
2023-03-25 12:44:39 -0400

    Improve plugin compatibility

--- src/ipad54/netherblocks/listener/EventListener.php
+++ src/ipad54/netherblocks/listener/EventListener.php
@@ -2,13 +2,22 @@
 
 namespace ipad54\netherblocks\listener;
 
+use ipad54\netherblocks\utils\CustomIds;
+use pocketmine\block\VanillaBlocks;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerCreationEvent;
-use ipad54\netherblocks\player\Player as MyPlayer;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\world\sound\FireExtinguishSound;
 
 class EventListener implements Listener {
 
-    public function onPlayerCreation(PlayerCreationEvent $event){
-        $event->setPlayerClass(MyPlayer::class);
-    }
+	public function onInteract(PlayerInteractEvent $event) : void {
+		$face = $event->getFace();
+		$target = $event->getBlock();
+		$block = $target->getSide($face);
+		$world = $target->getPosition()->getWorld();
+		if($block->getId() === CustomIds::SOUL_FIRE_BLOCK){
+			$world->setBlock($block->getPosition(), VanillaBlocks::AIR());
+			$world->addSound($block->getPosition()->add(0.5, 0.5, 0.5), new FireExtinguishSound());
+		}
+	}
 }
