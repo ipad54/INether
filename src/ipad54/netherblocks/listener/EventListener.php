482ed1cb3a10f10a2df0b2bf88bccd408594b01e
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-02 09:40:51 +1000

    Update EventListener.php

--- src/ipad54/netherblocks/listener/EventListener.php
+++ src/ipad54/netherblocks/listener/EventListener.php
@@ -1,22 +1,14 @@
+<?php
 
 namespace ipad54\netherblocks\listener;
 
-use ipad54\netherblocks\utils\CustomIds;
-use pocketmine\block\VanillaBlocks;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerInteractEvent;
-use pocketmine\world\sound\FireExtinguishSound;
+use pocketmine\event\player\PlayerCreationEvent;
+use ipad54\netherblocks\player\Player as MyPlayer;
 
 class EventListener implements Listener {
 
-	public function onInteract(PlayerInteractEvent $event) : void {
-		$face = $event->getFace();
-		$target = $event->getBlock();
-		$block = $target->getSide($face);
-		$world = $target->getPosition()->getWorld();
-		if($event->getAction() === PlayerInteractEvent::LEFT_CLICK_BLOCK && $block->getId() === CustomIds::SOUL_FIRE_BLOCK){
-			$world->setBlock($block->getPosition(), VanillaBlocks::AIR());
-			$world->addSound($block->getPosition()->add(0.5, 0.5, 0.5), new FireExtinguishSound());
-		}
-	}
+    public function onPlayerCreation(PlayerCreationEvent $event){
+        $event->setPlayerClass(MyPlayer::class);
+    }
 }
